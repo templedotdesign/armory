4317d2a6337b4a4931db4926cc2837b7e6507060
Ryan Temple <temple.design.me@gmail.com>
2017-11-08 00:03:48 -0500

    established basic structure

---
+++ app/src/components/receptions/receptions.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const receptions = () => {
+  return (
+    <div>
+      <h1>Receptions</h1>
+    </div>
+  );
+};
+
+export default receptions;
--- app/src/containers/app/App.js
+++ app/src/containers/app/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Header from '../../components/header/header';
+import Home from '../../components/home/home';
+import About from '../../components/about/about';
+import HistoryComponent from '../../components/history/history';
+import Receptions from '../../components/receptions/receptions';
+import Calendar from '../../components/calendar/calendar';
+import FAQ from '../../components/faq/faq'; 
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div>
-          App
+          <Header/>
+          <Switch>
+            <Route path="/about" component={About}/>
+            <Route path="/history" component={HistoryComponent}/>
+            <Route path="/receptions" component={Receptions}/>
+            <Route path="/calendar" component={Calendar}/>
+            <Route path="/faq" component={FAQ}/>
+            <Route path="/" component={Home}/>
+          </Switch>
         </div>
       </BrowserRouter>
     );
