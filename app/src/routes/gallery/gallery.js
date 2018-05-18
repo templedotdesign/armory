5d1346a5c58d4191b4847b7e9c1d33de7dd972f7
Ryan Temple <temple.design.me@gmail.com>
2018-05-18 12:27:44 -0400

    added new image to gallery

--- app/src/routes/gallery/gallery.js
+++ app/src/routes/gallery/gallery.js
@@ -5,29 +5,30 @@ import GalleryOverlay from './galleryOverlay/galleryOverlay';
 import classes from './gallery.css';
 
 import chandelierImg from '../../assets/chandelier.png';
-import armoryImg1 from '../../assets/armory_autumn.png';
-import armoryImg2 from '../../assets/armory_awnings.png';
-import armoryImg3 from '../../assets/armory_battlements.png';
+//import armoryImg1 from '../../assets/armory_autumn.png';
+//import armoryImg2 from '../../assets/armory_awnings.png';
+//import armoryImg3 from '../../assets/armory_battlements.png';
 import armoryImg4 from '../../assets/armory_car.png';
-import armoryImg5 from '../../assets/armory_gray.png';
-import armoryImg6 from '../../assets/armory_illustrated.png';
-import armoryImg7 from '../../assets/armory_sandman.png';
-import armoryImg8 from '../../assets/armory_tree.png';
-import armoryImg9 from '../../assets/armory_van.png';
+//import armoryImg5 from '../../assets/armory_gray.png';
+//import armoryImg6 from '../../assets/armory_illustrated.png';
+//import armoryImg7 from '../../assets/armory_sandman.png';
+//import armoryImg8 from '../../assets/armory_tree.png';
+//import armoryImg9 from '../../assets/armory_van.png';
 import coupleImg from '../../assets/couple.png';
-import dancingImg1 from '../../assets/dancing1.png';
-import dancingImg2 from '../../assets/dancing2.png';
-import dancingImg3 from '../../assets/dancing3.png';
+//import dancingImg1 from '../../assets/dancing1.png';
+//import dancingImg2 from '../../assets/dancing2.png';
+//import dancingImg3 from '../../assets/dancing3.png';
 import dancingImg4 from '../../assets/dancing4.png';
-import kissImg1 from '../../assets/kiss1.png';
+import ervinImg from '../../assets/ervin.jpg';
+//import kissImg1 from '../../assets/kiss1.png';
 import kissImg2 from '../../assets/kiss2.png';
 import tableImg from '../../assets/table.png';
 import tablesImg from '../../assets/tables.png';
 import couchImg from '../../assets/couch1.png';
-import barImg from '../../assets/bar.png';
-import mirrorImg from '../../assets/mirror.png'
-import facadeImg from '../../assets/facade.png';
-import gymImg from '../../assets/gym.png';
+//import barImg from '../../assets/bar.png';
+//import mirrorImg from '../../assets/mirror.png'
+//import facadeImg from '../../assets/facade.png';
+//import gymImg from '../../assets/gym.png';
 
 class Gallery extends Component {
   state = {
@@ -51,6 +52,7 @@ class Gallery extends Component {
       kissImg2,
       tableImg,
       couchImg,
+      ervinImg,
       tablesImg,
       // barImg,
       // mirrorImg,
